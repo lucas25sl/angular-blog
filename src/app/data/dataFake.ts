8dc1f1ab806628bbecff045967e87fedee50c32f
Lucas Pugliesi <lucas.pugliesi25@gmail.com>
2023-09-12 23:06:32 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO EM 3D",
+    "description":"Marvel anuncia novo filme do Homem de Ferro em 3d",
+    "photoCover":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"
+  },
+  {
+    "id":"2",
+    "title":"Lucas Moura humilha o Corinthians no Morumbi",
+    "description":"2x0 no Morumbi",
+    "photoCover":"https://placar.com.br/wp-content/uploads/2023/08/BAT7713-scaled.jpg"
+  }
+]
